d88e1efe89fc89fc67e4b52afcb675980b920c0e
Umang_Thadani <thadaniumang@outlook.com>
2022-05-06 00:12:52 +0530

    settings configured for deployment

--- linkme/settings.py
+++ linkme/settings.py
@@ -95,9 +95,17 @@ WSGI_APPLICATION = 'linkme.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config("PGDATABASE"),
+        'USER': config("PGUSER"),
+        'PASSWORD': config["PGPASSWORD"],
+        'HOST': config["PGHOST"],
+        'PORT': config["PGPORT"],
     }
 }
 
